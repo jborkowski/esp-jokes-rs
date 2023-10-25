2b0421ab68c2a6f4243ebd1364314d0c6ef6648f
Maciej Bielecki <zyla@prati.pl>
2023-10-25 09:47:34 +0200

    Display feature flag

--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,45 @@
 
 extern crate alloc;
 use core::{mem::MaybeUninit, str::from_utf8};
+use embassy_net::{
+    dns::DnsSocket,
+    tcp::client::{TcpClient, TcpClientState},
+    Config, Stack, StackResources,
+};
+use embassy_net::{Ipv4Address, Ipv4Cidr};
 use embassy_time::{Duration, Timer};
-use embedded_graphics::mono_font::MonoTextStyle;
 use embedded_graphics::mono_font::ascii::FONT_6X10;
+use embedded_graphics::mono_font::MonoTextStyle;
 use embedded_graphics::prelude::*;
 use embedded_graphics::text::Text;
 use embedded_graphics::{pixelcolor::Rgb565, prelude::RgbColor};
-use embedded_svc::wifi::{Configuration, ClientConfiguration, Wifi};
+use embedded_svc::wifi::{ClientConfiguration, Configuration, Wifi};
 use esp_backtrace as _;
 use esp_println::println;
-use hal::peripherals::SPI2;
-use hal::spi::FullDuplexMode;
-use hal::{embassy, clock::ClockControl, peripherals::Peripherals, prelude::*, Delay, Rtc, timer::TimerGroup, gpio::*, IO, Spi};
-use hal::{systimer::SystemTimer, Rng};
 use esp_wifi::wifi::{WifiController, WifiDevice, WifiEvent, WifiMode, WifiState};
 use esp_wifi::{initialize, EspWifiInitFor};
-use embassy_net::{Config, Stack, StackResources, tcp::client::{TcpClient, TcpClientState}, dns::DnsSocket};
-use embassy_net::{Ipv4Address, Ipv4Cidr};
+use hal::peripherals::SPI2;
+use hal::spi::FullDuplexMode;
+use hal::{
+    clock::ClockControl, embassy, gpio::*, peripherals::Peripherals, prelude::*, timer::TimerGroup,
+    Delay, Rtc, Spi, IO,
+};
+use hal::{systimer::SystemTimer, Rng};
 
-use heapless::Vec;
 use embedded_graphics::draw_target::DrawTarget;
-use reqwless::client::{TlsVerify, TlsConfig, HttpClient};
+use heapless::Vec;
+use reqwless::client::{HttpClient, TlsConfig, TlsVerify};
 use reqwless::headers::ContentType;
 use reqwless::request::{Method, RequestBuilder};
-use st7735_lcd::{self, ST7735};
-use st7735_lcd::Orientation;
 use static_cell::make_static;
 
 const SSID: &str = env!("SSID");
 const PASSWORD: &str = env!("PASSWORD");
 const IP_ADDRESS: Ipv4Cidr = Ipv4Cidr::new(Ipv4Address::new(192, 168, 50, 20), 24);
-const GATEWAY: Ipv4Address =  Ipv4Address::new(192, 168, 50, 1);
+const GATEWAY: Ipv4Address = Ipv4Address::new(192, 168, 50, 1);
 
 #[global_allocator]
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
-
 
 fn init_heap() {
     const HEAP_SIZE: usize = 32 * 1024;
@@ -49,8 +53,16 @@ fn init_heap() {
     }
 }
 
-type DISPLAY<'a> = ST7735<SPI, GpioPin<Output<PushPull>, 6>, GpioPin<Output<PushPull>, 7>>;
-type SPI = Spi<'static, SPI2, FullDuplexMode>;
+#[cfg(feature = "display-st7735")]
+mod display {
+    use super::*;
+    use st7735_lcd::{self, ST7735};
+
+    pub type SPI = Spi<'static, SPI2, FullDuplexMode>;
+    pub type DISPLAY<'a> = ST7735<SPI, GpioPin<Output<PushPull>, 6>, GpioPin<Output<PushPull>, 7>>;
+}
+
+use display::DISPLAY;
 
 #[embassy_executor::main(entry = "hal::entry")]
 async fn main(spawner: embassy_executor::Spawner) {
@@ -58,7 +70,7 @@ async fn main(spawner: embassy_executor::Spawner) {
     let peripherals = Peripherals::take();
     let mut system = peripherals.SYSTEM.split();
     let clocks = ClockControl::max(system.clock_control).freeze();
-    
+
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
     let timer_group0 = TimerGroup::new(
         peripherals.TIMG0,
@@ -73,8 +85,7 @@ async fn main(spawner: embassy_executor::Spawner) {
     );
     let mut wdt1 = timer_group1.wdt;
 
-
-    // disable watchdog timers 
+    // disable watchdog timers
     rtc.swd.disable();
     rtc.rwdt.disable();
     wdt0.disable();
@@ -88,19 +99,18 @@ async fn main(spawner: embassy_executor::Spawner) {
         rng,
         system.radio_clock_control,
         &clocks,
-    ).unwrap();
+    )
+    .unwrap();
 
     let (wifi, ..) = peripherals.RADIO.split();
     let (wifi_interface, controller) =
         esp_wifi::wifi::new_with_mode(&init, wifi, WifiMode::Sta).unwrap();
 
-
     embassy::init(&clocks, timer_group0.timer0);
 
-    
     let dhcp4_config = embassy_net::DhcpConfig::default();
     let config = Config::dhcpv4(dhcp4_config);
-   
+
     let seed = rng.random();
 
     let stack = &*make_static!(Stack::new(
@@ -109,64 +119,72 @@ async fn main(spawner: embassy_executor::Spawner) {
         make_static!(StackResources::<3>::new()),
         seed.into()
     ));
-    
 
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let input = io.pins.gpio9.into_pull_up_input();
 
-    hal::interrupt::enable(hal::peripherals::Interrupt::GPIO, hal::interrupt::Priority::Priority1).unwrap();
+    hal::interrupt::enable(
+        hal::peripherals::Interrupt::GPIO,
+        hal::interrupt::Priority::Priority1,
+    )
+    .unwrap();
 
-    /*
-    * The ST7735 display
-    */
+    use display::*;
 
-    let miso = io.pins.gpio6.into_push_pull_output(); // A0
-    let rst = io.pins.gpio7.into_push_pull_output();
+    #[cfg(feature = "display-st7735")]
+    let mut display: DISPLAY = {
+        let miso = io.pins.gpio6.into_push_pull_output(); // A0
+        let rst = io.pins.gpio7.into_push_pull_output();
 
-    let spi: SPI = Spi::new(
-        peripherals.SPI2,
-        io.pins.gpio1,
-        io.pins.gpio2, // sda
-        io.pins.gpio0.into_push_pull_output(), // dc not connected 
-        io.pins.gpio8,
-        60u32.MHz(),
-        hal::spi::SpiMode::Mode0,
-        &mut system.peripheral_clock_control,
-        &clocks,
-    );
-    
+        let spi: SPI = Spi::new(
+            peripherals.SPI2,
+            io.pins.gpio1,
+            io.pins.gpio2,                         // sda
+            io.pins.gpio0.into_push_pull_output(), // dc not connected
+            io.pins.gpio8,
+            60u32.MHz(),
+            hal::spi::SpiMode::Mode0,
+            &mut system.peripheral_clock_control,
+            &clocks,
+        );
 
-    let mut display: DISPLAY =
-        st7735_lcd::ST7735::new(spi, miso, rst, true, false, 160, 128);
+        let mut display = st7735_lcd::ST7735::new(spi, miso, rst, true, false, 160, 128);
 
-    let mut delay = Delay::new(&clocks);
-    display.init(&mut delay).unwrap();
+        let mut delay = Delay::new(&clocks);
+        display.init(&mut delay).unwrap();
+        display
+            .set_orientation(&st7735_lcd::Orientation::Landscape)
+            .unwrap();
+        display
+    };
 
-    display.set_orientation(&Orientation::Landscape).unwrap();
-    
     display.clear(Rgb565::BLACK).unwrap();
     display.set_offset(0, 0);
-    
+
     spawner.spawn(connection_wifi(controller)).ok();
     spawner.spawn(net_task(&stack)).ok();
-    spawner.spawn(task(input, &stack, seed.into(), display)).ok();
-   //  spawner.spawn(get_joke(&stack)).ok();
-
+    spawner
+        .spawn(task(input, &stack, seed.into(), display))
+        .ok();
+    //  spawner.spawn(get_joke(&stack)).ok();
 }
 
 #[embassy_executor::task]
-async fn task(mut input: Gpio9<Input<PullUp>>, stack: &'static Stack<WifiDevice<'static>>, seed: u64, mut display: DISPLAY<'_>) {
-
+async fn task(
+    mut input: Gpio9<Input<PullUp>>,
+    stack: &'static Stack<WifiDevice<'static>>,
+    seed: u64,
+    mut display: DISPLAY<'_>,
+) {
     let mut rx_buffer = [0; 8 * 1024];
-    let mut tls_read_buffer = [0; 8* 1024];
+    let mut tls_read_buffer = [0; 8 * 1024];
     let mut tls_write_buffer = [0; 8 * 1024];
     let client_state = TcpClientState::<4, 4096, 4096>::new();
     let tcp_client = TcpClient::new(&stack, &client_state);
     let dns = DnsSocket::new(&stack);
-    
+
     let style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
-    
-    
+
     loop {
         let _ = input.wait_for_any_edge().await;
         if input.is_high().unwrap() {
@@ -177,7 +195,7 @@ async fn task(mut input: Gpio9<Input<PullUp>>, stack: &'static Stack<WifiDevice<
                 }
                 Timer::after(Duration::from_millis(500)).await;
             }
- 
+
             println!("Waiting to get IP address...");
             loop {
                 if let Some(config) = stack.config_v4() {
@@ -189,34 +207,36 @@ async fn task(mut input: Gpio9<Input<PullUp>>, stack: &'static Stack<WifiDevice<
             display.clear(Rgb565::RED).unwrap();
             println!("clicked");
 
-            
-            let tls_config = TlsConfig::new(seed, &mut tls_read_buffer, &mut tls_write_buffer, TlsVerify::None);
+            let tls_config = TlsConfig::new(
+                seed,
+                &mut tls_read_buffer,
+                &mut tls_write_buffer,
+                TlsVerify::None,
+            );
             let mut http_client = HttpClient::new_with_tls(&tcp_client, &dns, tls_config);
-            let mut request =
-                http_client.request(Method::GET, "https://v2.jokeapi.dev/joke/Programming?format=txt")
-                    .await
-                    .unwrap();
-            
-            let response = request
-                .send(&mut rx_buffer)
+            let mut request = http_client
+                .request(
+                    Method::GET,
+                    "https://v2.jokeapi.dev/joke/Programming?format=txt",
+                )
                 .await
                 .unwrap();
 
+            let response = request.send(&mut rx_buffer).await.unwrap();
 
             let body = from_utf8(response.body().read_to_end().await.unwrap()).unwrap();
 
             display.clear(Rgb565::BLUE).unwrap();
 
-            Text::new(body, Point::new(10,10), style).draw(&mut display).unwrap();
+            Text::new(body, Point::new(10, 10), style)
+                .draw(&mut display)
+                .unwrap();
             println!("Http body: {}", body);
             Timer::after(Duration::from_millis(3000)).await;
             break;
-            
-        
         }
     }
 }
-
 
 #[embassy_executor::task]
 async fn connection_wifi(mut controller: WifiController<'static>) {
@@ -258,21 +278,21 @@ async fn net_task(stack: &'static Stack<WifiDevice<'static>>) {
 
 #[embassy_executor::task]
 async fn get_joke(stack: &'static Stack<WifiDevice<'static>>) {
-     loop {
-         if stack.is_link_up() {
-             break;
-         }
-         Timer::after(Duration::from_millis(500)).await;
-     }
-
-     println!("Waiting to get IP address...");
-     loop {
-        if let Some(config) = stack.config_v4() {
-          println!("Got IP: {}", config.address);
+    loop {
+        if stack.is_link_up() {
             break;
         }
         Timer::after(Duration::from_millis(500)).await;
-     }
+    }
+
+    println!("Waiting to get IP address...");
+    loop {
+        if let Some(config) = stack.config_v4() {
+            println!("Got IP: {}", config.address);
+            break;
+        }
+        Timer::after(Duration::from_millis(500)).await;
+    }
 
     println!("Got IP: {}", stack.is_config_up());
 }
